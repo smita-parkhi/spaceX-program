a3177efd57b779a34dc091f751002c8c9aa8f315
Smita Parkhi <smita.chande18@gmail.com>
2021-05-03 17:22:39 +0530

    create component Filter

--- src/common/filter-section/filter-section.component.js
+++ src/common/filter-section/filter-section.component.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import Card from '../card/card.component'
 
 import './filter-section.component.scss'
 
-export default function FilterSection() {
+export default function Filter({ filterHeading, options, activeFilterValue, lauchYearChangeCallback}) {
+
+    console.log(activeFilterValue)
+
+    const handleFilterClick = (filter) => {
+        lauchYearChangeCallback(filter)
+    }
+
     return (
         <div className='filter-wrapper'>
-            <h1>SpaceX Launch Program</h1>
             <div className='filter-section'>
-                <h3>Filters</h3>
-                <div className='filter-on-data'>
-                    <p>Launch Year</p>
-
-                </div>
+                <p className='heading'>{filterHeading}</p>
+                <ul className='options'>
+                    {options.map(filter => <li style={{ backgroundColor: (activeFilterValue && activeFilterValue.filterValue === filter.filterValue) ? 'red' : 'green' }}
+                        onClick={() => {
+                            handleFilterClick(filter);
+                        }}>
+                        <p>{filter.filterName}</p>
+                    </li>)}
+                </ul>
             </div>
-            <div className='card-section'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </div>
-
         </div>
     )
 }
--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,12 +1,88 @@
-import React from 'react';
-import FilterSection from '../../common/filter-section/filter-section.component';
+import React, { useEffect, useState } from 'react';
+import Filter from '../../common/filter-section/filter-section.component';
+import Card from '../../common/card/card.component';
 
 import './home.component.scss'
 
-export default function Home(){
-    return(
+
+
+export default function Home() {
+
+    const [activeLanuchYear, setActiveLaunchYear] = useState(undefined)
+    const [isSuccessfulLaunch, setSuccessfulLaunch] = useState(undefined)
+    const [isSuccessfulLand, setSuccessfulLand] = useState(undefined)
+
+    useEffect(() => {
+        console.log('cal api')
+    }, [activeLanuchYear, isSuccessfulLaunch, isSuccessfulLand])
+
+
+    const launchYearFilter = [
+        { id: 1, filterName: 2006, filterValue: 2006 },
+        { id: 2, filterName: 2007, filterValue: 2007 },
+        { id: 3, filterName: 2008, filterValue: 2008 },
+    ]
+
+    const successfulLaunchFilter = [
+        { id: 1, filterName: 'True', filterValue: true },
+        { id: 2, filterName: 'False', filterValue: false }
+    ]
+
+    const successfulLandFilter = [
+        { id: 1, filterName: 'True', filterValue: true },
+        { id: 2, filterName: 'False', filterValue: false }
+    ]
+
+    const handleLauchYearChange = (activeFilter) => {
+        setActiveLaunchYear(activeFilter)
+        setSuccessfulLaunch(activeFilter)
+        setSuccessfulLand(activeFilter)
+    }
+
+    const handleSuccessfulLaunch = (filter) => {
+        console.log(filter)
+        setSuccessfulLaunch(filter)
+    }
+
+    const handleSuccessfulLand = (activeFilter) => {
+        setSuccessfulLand(activeFilter)
+    }
+
+
+    return (
         <div className='home-wrapper'>
-            <FilterSection />
+            <div className='filter'>
+                <Filter
+                    filterHeading='Launch Year'
+                    options={launchYearFilter}
+                    activeFilterValue={activeLanuchYear}
+                    lauchYearChangeCallback={(year) => handleLauchYearChange(year)}
+                />
+
+                <Filter
+                    filterHeading='Successful Launch'
+                    activeFilterValue={isSuccessfulLaunch}
+                    options={successfulLaunchFilter}
+                    lauchYearChangeCallback={(year) => handleSuccessfulLaunch(year)}
+                />
+
+                <Filter
+                filterHeading='Successful Land'
+                activeFilterValue={isSuccessfulLand}
+                options={successfulLandFilter}
+                lauchYearChangeCallback={(year) => handleSuccessfulLand(year)}
+            />
+            </div>
+
+            <div className='card-section'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+
         </div>
     )
 }
